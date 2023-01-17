64dd2c2bc53bd90cd426a5b6034371bdd6196750
echizen8 <62060135+echizen8@users.noreply.github.com>
2023-01-17 19:19:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+const images = require.context('./images', true);
+const imageKeys = images.keys();
+
+const App = () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [displayedImages, setDisplayedImages] = useState([]);
+
+  useEffect(() => {
+    let randomIndex = Math.floor(Math.random() * imageKeys.length);
+    while (displayedImages.includes(randomIndex)) {
+      randomIndex = Math.floor(Math.random() * imageKeys.length);
+    }
+    setDisplayedImages([...displayedImages, randomIndex]);
+    setCurrentImageIndex(randomIndex);
+  }, []);
+
+  useEffect(() => {
+    if (displayedImages.length === imageKeys.length) {
+      setDisplayedImages([]);
+    }
+  }, [displayedImages]);
+
+
+  return (
+    <div className="app-container">
+      <img
+        className="centered-image"
+        src={images(imageKeys[currentImageIndex])}
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default App;
